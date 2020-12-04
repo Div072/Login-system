b979d3c084e4f8db60d45aaa035ddaac3cbdfb99
Div072 <pdivyesh072@gmail.com>
2020-12-04 23:30:44 +0530

    1

--- app/views.py
+++ app/views.py
@@ -3,22 +3,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from django.core.exceptions import ObjectDoesNotExist
+
 # Create your views here.
 
 
 def index(request):
-    return render(request,'app/index.html')
-
+    return render(request, "app/index.html")
 
 
 def login(request):
-    if request.method=='POST':
+    if request.method == "POST":
         try:
-            q = User.objects.get(pk = request.POST['Email'])
+            q = User.objects.get(pk=request.POST["Email"])
 
         except ObjectDoesNotExist:
             return HttpResponse("Null")
         return HttpResponse(q.Email)
     else:
-        return HttpResponse("GET")  
-            
+        return HttpResponse("GET")
