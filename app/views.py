53b333c60bb41236b20e0d200fc29dff151b5828
Div072 <pdivyesh072@gmail.com>
2020-12-05 17:50:39 +0530

    1.2

--- app/views.py
+++ app/views.py
@@ -8,16 +8,26 @@ from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request):
-    return render(request, "app/index.html")
-
-
-def login(request):
     if request.method == "POST":
         try:
             q = User.objects.get(pk=request.POST["Email"])
 
         except ObjectDoesNotExist:
-            return HttpResponse("Null")
-        return HttpResponse(q.Email)
-    else:
-        return HttpResponse("GET")
+            contex = {"error": "Invalid Email"}
+            print(contex)
+            return render(request, "app/index.html", contex)
+        return render(request, "app/loging")
+
+    elif request.method == "GET":
+        return render(request, "app/index.html")
+
+
+def register(request):
+    if request.method == "POST":
+        q = User(
+            Name="Div", Email=request.POST["Email"], Password=request.POST["Password"]
+        )
+        q.save()
+        return HttpResponse("POST")
+    elif request.method == "GET":
+        return render(request, "app/register.html")
